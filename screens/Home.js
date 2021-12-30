99da34cbe7061a4bcb64943a396652f1e872ccc2
Ozan <cetino19@itu.edu.tr>
2021-12-30 17:48:33 +0300

    react-native-safe-area-context / view issue solved

--- screens/Home.js
+++ screens/Home.js
@@ -1,27 +1,34 @@
 import React, {useState} from 'react'
-import { View, Text, SafeAreaView, ScrollView } from 'react-native'
+import { View, Text, ScrollView } from 'react-native'
 import Categories from '../components/Categories'
 import Header from '../components/Header'
 import FoodCard from '../components/FoodCard'
+import { Divider } from 'react-native-elements'
+import BottomNav from '../components/BottomNav'
+import SafeAreaView from 'react-native-safe-area-view';
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 
 export default function Home() {
 
     const [activeTab, setActiveTab] = useState("Delivery"); 
     
     return (
-        <SafeAreaView style={{backgroundColor: "#eee"}}>
-            <View style = {{backgroundColor: "white"}}>
-                <Header activeTab={activeTab} setActiveTab={setActiveTab}/>   
-            </View>
+        <SafeAreaProvider>
             
-            <ScrollView showsVerticalScrollIndicator={false}>
-                <Categories/>
-                <FoodCard activeTab = {activeTab} setActiveTab = {setActiveTab}/>
-            </ScrollView>
+            <SafeAreaView style={{backgroundColor: "#eee"}}>
+                <View style = {{backgroundColor: "white"}}>
+                    <Header activeTab={activeTab} setActiveTab={setActiveTab}/>   
+                </View>
+                
+                <ScrollView showsVerticalScrollIndicator={false}>
+                    <Categories/>
+                    <FoodCard activeTab = {activeTab} setActiveTab = {setActiveTab}/>
+                </ScrollView>
+                <Divider width={1} />
+                <BottomNav/>
 
-            
-
-
-        </SafeAreaView>
+            </SafeAreaView>
+        
+        </SafeAreaProvider>
     )
 }
