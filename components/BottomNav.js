2a3368d64ec2f7364a1aea814c76cd4db4dd2ffe
Ozan <cetino19@itu.edu.tr>
2021-12-30 18:24:21 +0300

    bottom nav nearly finished

--- components/BottomNav.js
+++ components/BottomNav.js
@@ -1,10 +1,39 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import FontAwesome5 from "react-native-vector-icons/FontAwesome5"; 
+import { TouchableOpacity } from 'react-native-web';
+
 
 export default function BottomNav() {
     return (
-        <View>
-            <Text>BOTTOM</Text>
+        <View style = {{
+            flexDirection: "row", 
+            margin: 10, 
+            marginHorizontal: 30,
+            justifyContent: "space-between"
+        }}>
+            {/* Home Icon */}
+            <Icon icon = "home"  /> 
+            <Icon icon = "shopping-cart" />
+            <Icon icon = "qrcode" />
+            <Icon icon = "comment-dots" />
+            <Icon icon = "user-circle" />
         </View>
     )
 }
+
+const Icon = (props) =>  ( 
+    <TouchableOpacity>
+        <View>
+            <FontAwesome5 
+            name = {props.icon} 
+            size = {25}
+            style= {{
+                marginBottom: 3, 
+                alignSelf: "center"
+            }} 
+            />
+        </View>
+    </TouchableOpacity>
+    
+); 
