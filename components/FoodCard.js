070e1c80c80b2e99394029fa41dc8fbd08dcec4e
Ozan <cetino19@itu.edu.tr>
2021-12-30 12:27:19 +0300

    developing the foodcards

--- components/FoodCard.js
+++ components/FoodCard.js
@@ -2,64 +2,145 @@ import React from 'react'
 import { View, Text, Image, ScrollView} from 'react-native'
 import { TouchableOpacity } from 'react-native-web';
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons"; 
+
+const suggestions = [
+    {
+        categoryName: "Bakery",
+        restaurantName: "Besiktas Burger King",
+        foodName: "Big King",
+        foodImage: "https://images.unsplash.com/photo-1600891964599-f61ba0e24092?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmVzdGF1cmFudCUyMGZvb2R8ZW58MHx8MHx8&w=1000&q=80", 
+        price: "32$",
+        rating: "4.2", 
+    }, 
+
+     {
+        categoryName: "Fast Food",
+        restaurantName: "Papa John's",
+        foodName: "Orta Boy Pizza",
+        foodImage: "https://cdn.getiryemek.com/restaurants/1581323800278_1125x522.jpeg",
+        price: "22$",
+        rating: "4.4", 
+    }, 
+     {
+        categoryName: "Bakery",
+        restaurantName: "Besiktas Burger King",
+        foodName: "Big King",
+        foodImage: "https://images.unsplash.com/photo-1600891964599-f61ba0e24092?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmVzdGF1cmFudCUyMGZvb2R8ZW58MHx8MHx8&w=1000&q=80", 
+        price: "32$",
+        rating: "4.2", 
+    }, 
+     {
+        categoryName: "Bakery",
+        restaurantName: "Besiktas Burger King",
+        foodName: "Big King",
+        foodImage: "https://images.unsplash.com/photo-1600891964599-f61ba0e24092?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmVzdGF1cmFudCUyMGZvb2R8ZW58MHx8MHx8&w=1000&q=80", 
+        price: "32$",
+        rating: "4.2", 
+    }, 
+     {
+        categoryName: "Bakery",
+        restaurantName: "Besiktas Burger King",
+        foodName: "Big King",
+        foodImage: "https://images.unsplash.com/photo-1600891964599-f61ba0e24092?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmVzdGF1cmFudCUyMGZvb2R8ZW58MHx8MHx8&w=1000&q=80", 
+        price: "32$",
+        rating: "4.2", 
+    }, 
+     {
+        categoryName: "Bakery",
+        restaurantName: "Besiktas Burger King",
+        foodName: "Big King",
+        foodImage: "https://images.unsplash.com/photo-1600891964599-f61ba0e24092?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmVzdGF1cmFudCUyMGZvb2R8ZW58MHx8MHx8&w=1000&q=80", 
+        price: "32$",
+        rating: "4.2", 
+    }, 
+  
+
+]
 export default function RestaurantCard() {
     return (
 
-        <ScrollView showsVerticalScrollIndicator= {false}> {/* Only Vertical ScrollView */}
+        <ScrollView showsVerticalScrollIndicator= {false}
+            style={{backgroundColor: "white"}}
+        > {/* Only Vertical ScrollView */}
             
             {/* More than one Horizontal ScrollViews */}
+
+
+
+                 {/* horizontal scrollview for one category*/}
+                
+                <View>
+                        <Text
+                            style={{fontSize: 21, color: "rgb(247,80,0)", fontWeight: "600"}}
+                        >
+                            Bakery
+                            </Text>
+                </View>
+
+
                 <ScrollView horizontal showsHorizontalScrollIndicator={false}
+                    style={{flexDirection: "column"}}
                 >
-                    <View style={{
+                   
+                   {suggestions.map((suggestion, index) => (
+                       <TouchableOpacity style={{
                         alignItems: "center",
                         marginRight:"30",   
-                        paddingHorizontal: 50, 
+                        paddingHorizontal: 120,
+                        borderRadius: "30" 
+                        
                     }}>
-                        <FoodImage/>
-                        <FoodInfo/>
-                    </View>
-                    <View style = {{
-                        alignItems: "center",
-                        marginRight:"30",
-                        paddingHorizontal: 50, 
-                    }}>
-                        <FoodImage/>
-                        <FoodInfo/>
-                    </View>
-                </ScrollView>      
+                        <FoodHeader restaurantName = {suggestion.restaurantName}/>
+                        <FoodImage url = {suggestion.foodImage} />
+                        <FoodInfo price = {suggestion.price} foodName = {suggestion.foodName} />
+
+                       </TouchableOpacity>
+                   ))}
+                
+                 
+                </ScrollView>
+
+                
+             
         
         </ScrollView>
     )
 }
 
-const FoodImage = () => (
+const FoodImage = (props) => (
     <>
         <Image
             source = {{
-                uri: "https://images.unsplash.com/photo-1600891964599-f61ba0e24092?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmVzdGF1cmFudCUyMGZvb2R8ZW58MHx8MHx8&w=1000&q=80"
+                uri: `${props.url}`
             }}
             style = {{
-                width: "150%", height: 180,
+                width: "250%", height: 160,
             }}
         />
-        <TouchableOpacity style = {{position: "absolute", left: "20", bottom:"20"}}>
+        <TouchableOpacity style = {{position: "absolute"}}>
             <MaterialCommunityIcons name = "heart-outline" size={30} color = "#fff"/>
         </TouchableOpacity>
     </>
 );
 
-const FoodInfo = () => ( 
+const FoodInfo = (props) => ( 
     <View 
     style = {{
         flexDirection: "row", 
         justifyContent: "space-between", 
         alignItems: "center", 
-        marginTop: 10,
+        marginTop: 5,
     }}>
 
     <View>
-        <Text>Rumelihisari İskele Restoran</Text>
+        <Text>{props.foodName}</Text>
+        <Text>{props.price}</Text>
+        <Text>{props.rating}</Text>
     </View>
 
     </View>
+)
+
+const FoodHeader= (props) => (
+    <View><Text>{props.restaurantName}</Text></View>
 )
